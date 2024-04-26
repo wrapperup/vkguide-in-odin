0cdcb8970bd7029f0a827c952af7dd7e041d665e
wrapperup <wrapperup4@gmail.com>
2024-04-26 00:58:42 -0400

    chapter 3.3: shaders

--- build-shaders.ps1
+++ build-shaders.ps1
@@ -18,3 +18,25 @@ slangc shaders/sky.comp.slang `
     -target spirv `
     -capability spirv_1_6 `
     -o shaders/out/sky.comp.spv
+
+slangc shaders/colored_triangle.vert.slang `
+    -entry main `
+    -profile sm_6_0 `
+    -target spirv `
+    -capability spirv_1_6 `
+    -o shaders/out/colored_triangle.vert.spv
+
+slangc shaders/colored_triangle.frag.slang `
+    -entry main `
+    -profile sm_6_0 `
+    -target spirv `
+    -capability spirv_1_6 `
+    -o shaders/out/colored_triangle.frag.spv
+
+slangc shaders/colored_triangle_mesh.vert.slang `
+    -entry main `
+    -profile sm_6_0 `
+    -target spirv `
+    -capability spirv_1_6 `
+    -emit-spirv-directly `
+    -o shaders/out/colored_triangle_mesh.vert.spv
